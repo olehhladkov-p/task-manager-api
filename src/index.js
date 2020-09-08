bb7a2feef300d5f7587f78bbab1119f6aac5936f
olehhladkov <oleh.hladkov@gmail.com>
2020-09-08 21:22:18 +0300

    Init commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('./db/mongoose');
+
+const express = require('express');
+
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
+});
